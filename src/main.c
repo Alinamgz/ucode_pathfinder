ae44eec47ace19bff880b8b2aca2b6ed341fc12f
AlinaMgz <alinamgz@ukr.net>
2020-10-13 18:35:57 +0300

    added file_validation func

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 int main(int arc, char *arv[]) {
     char *file = NULL;
     char **file_arr = NULL;
+    bool is_valid = 1;
 
     if (arc != 2) {
         mx_printerr(USAGE_ERR);
@@ -11,19 +12,23 @@ int main(int arc, char *arv[]) {
 
     file = mx_file_to_str(arv[1]);
 
-    if (file == NULL) {
-        mx_printerr(NONEXISTENT);
-        exit(1);
-    }
-    else if (mx_strlen(mx_strtrim(file)) <= 0) {
-        mx_printerr(FILE_ERR);
-        mx_printerr(arv[1]);
-        mx_printerr(EMPTY);
+    is_valid = mx_file_validation(file);
+    if (is_valid == FALSE) {
         mx_strdel(&file);
         exit(1);
-    } 
-    
-mx_printint(mx_strlen(file));
+    }
+
+    // if (file == NULL) {
+    //     mx_printerr(NONEXISTENT);
+    //     exit(1);
+    // }
+    // else if (mx_strlen(mx_strtrim(file)) <= 0) {
+    //     mx_printerr(FILE_ERR);
+    //     mx_printerr(arv[1]);
+    //     mx_printerr(EMPTY);
+    //     mx_strdel(&file);
+    //     exit(1);
+    // } 
 
     file_arr = mx_strsplit(file, '\n');
     mx_strdel(&file);
--- src/mx_validation.c
+++ src/mx_validation.c
@@ -1,5 +1,22 @@
 #include "pathfinder.h"
 
+bool mx_file_validation(const char *file) {
+    bool rslt = TRUE;
+
+    if (file == NULL) {
+        rslt = FALSE;
+        mx_printerr(NONEXISTENT);
+    }
+    else if (mx_strlen(mx_strtrim(file)) <= 0) {
+        rslt = FALSE;
+        mx_printerr(FILE_ERR);
+        mx_printerr(arv[1]);
+        mx_printerr(EMPTY);
+    } 
+
+	return rslt;
+}
+
 bool mx_validation(char **arr) {
     bool rslt = 1;
 
