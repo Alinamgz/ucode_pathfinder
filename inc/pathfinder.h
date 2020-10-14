3d2cce1c580df9c474865e1a4afba462433036ba
AlinaMgz <alinamgz@ukr.net>
2020-10-14 20:04:50 +0300

    edited check

--- inc/pathfinder.h
+++ inc/pathfinder.h
@@ -28,5 +28,6 @@
 
 // ======= funcs =======
 bool mx_validation(char **arr);
-bool mx_file_validation(char *file, char *file_name);
+// bool mx_file_validation(char *file, char *file_name);
+bool mx_file_validation(char *file_name);
 void mx_err_exit(char **file, char ***file_arr);
--- src/mx_validation.c
+++ src/mx_validation.c
@@ -1,23 +1,46 @@
 #include "pathfinder.h"
 
-bool mx_file_validation(char *file, char *file_name) {
-    bool rslt = TRUE;
-    int err = 0;
+// bool mx_file_validation(char *file, char *file_name) {
+//     bool rslt = TRUE;
+//     int err = 0;
 
-    if (file == NULL) {
+//     if (file == NULL) {
+//         err = 1;
+//     }
+//     else if (file[0] == '\0') {
+//         err = 2;
+//     }
+
+//     if (err) {
+//         rslt = FALSE;
+//         mx_printerr(FILE_ERR);
+//         mx_printerr(file_name);
+//         err == 1 ? mx_printerr(NONEXISTENT) : mx_printerr(EMPTY);
+
+//     }
+// 	return rslt;
+// }
+
+bool mx_file_validation(char *file_name) {
+    int err = 0;
+    int file = open(file_name, O_RDONLY);
+    char *buf = NULL;
+
+    if (file < 0) {
         err = 1;
     }
-    else if (file[0] == '\0') {
-        err = 2;
+    else {
+        read(file, &buf, 32);
+        err = buf ? err : 2;
     }
+    close(file);
 
     if (err) {
-        rslt = FALSE;
         mx_printerr(FILE_ERR);
         mx_printerr(file_name);
         err == 1 ? mx_printerr(NONEXISTENT) : mx_printerr(EMPTY);
-
+        return FALSE;
     }
-	return rslt;
+	return TRUE;
 }
 
