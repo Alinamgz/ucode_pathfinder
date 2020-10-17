b99e09be4ede90e5a87d745ed0053670166968cd
AlinaMgz <alinamgz@ukr.net>
2020-10-17 16:35:16 +0300

    spl draft

--- src/mx_split_arr.c
+++ src/mx_split_arr.c
@@ -2,7 +2,12 @@
 
 char ***mx_split_arr(char **file_arr) {
     char ***rslt = NULL;
+    char del_a = '-';
+    char del_b = ',';
     int size = 0;
+    int row_sz = 0;
+    char **buf_a = NULL;
+    char **buf_b = NULL;
 
     while(file_arr[size]){
         size++;
@@ -15,8 +20,43 @@ char ***mx_split_arr(char **file_arr) {
         if (i == size) {
             rslt[i] = 0;
         }
+        else if (i == 0) {
+            row_sz = 2;
+            rslt[i] = (char **)malloc(sizeof(char *) * row_sz);
+            for (int j = 0; j < row_sz - 1; j++){
+                if(j == row_sz-1){
+                    rslt[i][j] = 0;
+                }
+                else {
+                    mx_strdup(file_arr[j]);
+                }
+            }
+        }
         else {
-            rslt[i] = mx_strsplit(file_arr[i], ',');
+            row_sz = 4;
+            rslt[i] = (char**)malloc(sizeof(char*) * row_sz);
+            for (int j = 0; j < row_sz - 1; j++) {
+                if (j == row_sz-1) {
+                    rslt[i][j] = 0;
+                }
+                else {
+ buf_a = mx_strsplit(file_arr[i], del_a);
+                    buf_b = mx_strsplit(buf_a[1], del_b);
+                    for (int j = 0; j < row_sz - 1; j++){
+                        switch (j) {
+                            case 0:
+                                rslt[i][j] = mx_strdup(buf_a[j]);
+                                break;
+                            case (row_sz - 1):
+                                rslt[i][j] = 0;
+                                break;
+                            default:
+                                rslt[i][j] = mx_strdup(buf_b[j - 1]);
+                                break;
+                        }
+                    }
+                }
+            }
         }
     }
 
