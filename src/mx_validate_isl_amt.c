ae7161215999e90ebb0c36c521cf196629f1a071
Alina Mazur <amazur@e3r6p5.unit.ua>
2020-11-03 14:54:28 +0200

    islands amt check

--- src/mx_validate_isl_amt.c
+++ src/mx_validate_isl_amt.c
@@ -2,30 +2,38 @@
 
 void mx_validate_isl_amt(t_validation *v) {
     v->amt = mx_atoi(v->file_strarr[0]);
-    v->islands = (char**)malloc(sizeof(char*) * (1+ 2 * v->lines));
-    
-    for(v->j = 2 * v->lines; v->j > 0; v->j-- ) {
+    v->islands = (char**)malloc(sizeof(char*) * (1 + v->amt));
+    bool unique = 1;
+
+    for(v->j = 1 + v->amt; v->j > 0; v->j-- ) {
         v->islands[v->j] = NULL;
     }
     v->islands[v->j] = NULL;
 
-mx_printint(v->j);
-
-    for( ;v->i > 0; v->i--) {
-mx_printstr("\ncheck\n");
+    for(v->i = 1;v->i < v-> lines; v->i++) {
         v->buf_dist = mx_strsplit(v->file_strarr[v->i], ',');
         v->buf_isl = mx_strsplit(v->buf_dist[0], '-');
         v->dist_tot += mx_atoi(v->buf_dist[1]);
         for (int a = 0; v->buf_isl[a]; a++) {
-            v->islands[v->j] = mx_strdup(v->buf_isl[a]);
-            v->j++;
+            unique = 1;
+            for(int b = 0; v->islands[b]; b++) {
+                if(mx_strcmp(v->islands[b], v->buf_isl[a]) == 0) {
+                    unique = 0;
+                    break;
+                }
+            }
+            if (unique) {
+                if (v->j < v->amt) {
+                v->islands[v->j] = mx_strdup(v->buf_isl[a]);
+                }
+                   v->j++;
+            }
         }
         mx_del_strarr(&v->buf_dist);
         mx_del_strarr(&v->buf_isl);
     }
 
-for(int i = 0; v->islands[i]; i++) {
-    mx_printstr(v->islands[i]);
-    mx_printchar(DELIM);
-}
+    if (v->j != v->amt) {
+        mx_printerr(ISLANDS_AMT_ERR);
+    }
 }
